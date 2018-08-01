3e2e6a0724f0a01e31f34435eedaedfcefc6dd57
Samuel Kim <sams404link@gmail.com>
2018-08-01 01:49:20 -0700

    fixed homepage fonts

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,8 +10,8 @@ export default class HomePage extends Component {
         <Navbar />
         <div className = 'Page-Container'>
         <h1 className="app-title-item video-title"> <b> Welcome to  Videorealm</b> </h1>
-        <h4 className='app-title-item'>Your next social media app!<br/><br/></h4>
-        <h4 className='app-title-item'><br />Latest Videos </h4>
+        <h4 className='app-title-item'>Your next social media app!</h4>
+        <h1 className='app-title-item'><br />Latest Videos </h1>
           <AllVideos/>
         </div>
       </div>
