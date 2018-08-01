4532bc674df284cf31c325f036ddf21b671b08a6
Samuel Kim <sams404link@gmail.com>
2018-08-01 01:08:58 -0700

    added more error message for create user

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -24,31 +24,31 @@ export default class CreateUser extends Component {
     if (this.state.username.length < 3) {
       badUsername = true;
       const userDoc = document.getElementById('shortUsername');
-      userDoc.style.display = "block";
+      userDoc.style.display = 'block';
     } else {
       const userDoc = document.getElementById('shortUsername');
-      userDoc.style.display = "none";
+      userDoc.style.display = 'none';
     }
 
     //check if email exists
     if (!EmailValidator.validate(this.state.email)) {
       const emailDoc = document.getElementById('badEmail');
-      emailDoc.style.display = "block";
+      emailDoc.style.display = 'block';
       badEmail = true;
     } else {
       const emailDoc = document.getElementById('badEmail');
-      emailDoc.style.display = "none";
+      emailDoc.style.display = 'none';
     }
     //check password if it meets requirements
     if (!/\d/.test(this.state.password) || !/\d/.test(this.state.password) || 
     this.state.password.length < 6 || this.state.password.length > 20) {
       const paswordDoc = document.getElementById('badPassword');
-      paswordDoc.style.display = "block";
+      paswordDoc.style.display = 'block';
       badPassword = true;
     
     } else {
       const paswordDoc = document.getElementById('badPassword');
-      paswordDoc.style.display = "none";
+      paswordDoc.style.display = 'none';
     }
 
     const usernameReq = {username: this.state.username};
@@ -58,22 +58,22 @@ export default class CreateUser extends Component {
       .then((data) => {
         if (data.data.error) {
           const userDoc = document.getElementById('badUsername');
-          userDoc.style.display = "block";
+          userDoc.style.display = 'block';
           badUsername = true;
         } else {
           const userDoc = document.getElementById('badUsername');
-          userDoc.style.display = "none";
+          userDoc.style.display = 'none';
         }
         axios.post(`${reqURL}/checkEmail`, emailReq)
           .then((data) => {
             if (data.data.error) {
               console.log('error', data.data.err)
               const emailDoc = document.getElementById('dupEmail');
-              emailDoc.style.display = "block";
+              emailDoc.style.display = 'block';
               badEmail = true;
             } else {
               const emailDoc = document.getElementById('dupEmail');
-              emailDoc.style.display = "none";
+              emailDoc.style.display = 'none';
             }
 
             // if it passes all tests 
@@ -120,30 +120,39 @@ export default class CreateUser extends Component {
   handleSetEmail = (event) => {
     this.setState({ email: event.target.value });
   }
+  componentDidUpdate() {
+    let eventEnter = document.getElementById('page-event');
+    eventEnter.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        this.createUser();
+      }
+    })
+  }
   render() {
     return (
-      <div>
+      <div id = 'page-event'>
         <Navbar />
         <div className = 'Page-Container'>
-          <div className="form-group app-userform">
+          <div className='form-group app-userform'>
             <h1 className = 'app-title-item'> Create Your Videorealm Account </h1>
-            <label htmlFor="name"> Choose your username:</label>
-            <input type="name" className="form-control" id="name" value = { this.state.username } onChange = { this.handleSetUsername }/>
-            <p id = 'shortUsername' className ="email-Warning">Error: Username must be at least 3 letters</p> 
-            <p id = 'badUsername' className ="email-Warning">Error: Username already exists</p> 
+            <label htmlFor='name' id='username-message'> <b>Choose your username:</b></label>
+            <input type='name' className='form-control' id='name' value = { this.state.username } onChange = { this.handleSetUsername }/>
+            <p id = 'shortUsername' className ='email-warning'>Error: Username must be at least 3 letters</p> 
+            <p id = 'badUsername' className ='email-warning'>Error: Username already exists</p> 
           </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Create your password:</label>
-            <input type="password" className="form-control" id="pwd" value = { this.state.password } onChange = { this.handleSetPassword }/>
-            <p id = 'badPassword' className ="email-Warning">Error: invalid password</p> 
+          <div className='form-group'>
+            <label htmlFor='pwd'><b>Create your password:</b></label>
+            <input type='password' className='form-control' id='pwd' value = { this.state.password } onChange = { this.handleSetPassword }/>
+            <p id = 'badPassword' className ='email-warning'>Error: invalid password. Password must be at least 8 characters long and must 
+            include at least one number and one special character </p> 
           </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Email:</label>
-            <input type="email" className="form-control" id="email" value = { this.state.email } onChange = { this.handleSetEmail }/>
+          <div className='form-group'>
+            <label htmlFor='pwd'><b>Enter a valid Email:</b></label>
+            <input type='email' className='form-control' id='email' value = { this.state.email } onChange = { this.handleSetEmail }/>
+            <p id = 'badEmail' className ='email-warning'>Please enter an existing email</p>
+            <p id = 'dupEmail' className ='email-warning'>Error: Only one account allowed per email</p> 
           </div>
-          <p id = 'badEmail' className ="email-Warning">Please enter an existing email!</p>
-          <p id = 'dupEmail' className ="email-Warning">Error: Only one account allowed per email</p> 
-          <button type="submit" className="btn btn-default" onClick = { this.createUser }>Submit</button>
+          <button type='submit' className='btn btn-default' onClick = { this.createUser }>Submit</button>
         </div>
       </div>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -13,8 +13,7 @@ export default class Account extends Component {
       password:'',
     }
   }
-  loginUser = (event) => {
-    event.preventDefault();
+  loginUser = () => {
     const user = { username: this.state.username, password: this.state.password };
     axios.post(`${reqURL}/mongoLogin`, user)
     .then((data) => {
@@ -35,24 +34,34 @@ export default class Account extends Component {
   handleSetPassword = (event) => {
     this.setState({ password: event.target.value });
   }
+  checkEnterKey = (event) => {
+    const keyPressed = event.key;
+    console.log(keyPressed);
+  }
+  componentDidUpdate() {
+    let eventEnter = document.getElementById('page-event');
+    eventEnter.addEventListener('keypress', (event) => {
+      console.log(`first keydown event. key property value is "${event.key}"`);
+      if (event.key === 'Enter') {
+        this.loginUser();
+      }
+    })
+  }
   render() {
     return(
-      <div>
+      <div id='page-event'>
         <Navbar />
-        <div className = 'Page-Container'>
+        <div className = 'Page-Container ' onkeydown={this.checkEnterKey}>
           <h1 className= "app-title-item">Login to Videorealm</h1>
           <div className="form-group">
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name"><b>Enter your userame:</b></label>
           <input type="name" className="form-control" id="name" value = { this.state.username } onChange = { this.handleSetUsername }/>
         </div>
         <div className="form-group">
-          <label htmlFor="pwd">Password:</label>
+          <label htmlFor="pwd"><b>Enter your Password:</b></label>
           <input type="password" className="form-control" id="pwd" value = { this.state.password } onChange = { this.handleSetPassword }/>
         </div>
-        <p id = 'badLogin' className ="email-Warning">Error: Incorrect username and/or password! </p> 
-        <div className="checkbox">
-          <label><input type="checkbox"/> Remember me</label>
-        </div>
+        <p id = 'badLogin' className ="email-warning">Error: Incorrect username and/or password! </p> 
           <button type="submit" className="btn btn-default" onClick = { this.loginUser }>Submit</button>
         </div>
       </div>
