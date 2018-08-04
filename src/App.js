75b659e8052a8d77e15742853b2805b0562a2209
Samuel Kim <sams404link@gmail.com>
2018-08-03 22:48:26 -0700

    moved routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Profile from './components/Profile';
+import Account from './components/Account';
+import Login from './components/Login';
+import CreateUser from './components/CreateUser';
+import AdminPage from './components/AdminPage';
+import VideoPlayer from './components/VideoPlayer';
+import SearchPage from './components/SearchPage';
+import ErrorPage from './components/Errorpage';
 import './CSS/App.css';
 
 axios.defaults.withCredentials = true;
 
 export default function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/profile/:username" component={Profile} />
+        <Route exact path="/account" component={Account} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/createUser" component={CreateUser} />
+        <Route exact path="/adminpage" component={AdminPage} />
+        <Route exact path="/video/:videoID" component={VideoPlayer} />
+        <Route exact path="/video_search/:searchterm" component={SearchPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,35 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import Thunk from 'redux-thunk';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Profile from './components/Profile';
-import Account from './components/Account';
-import Login from './components/Login';
-import CreateUser from './components/CreateUser';
-import AdminPage from './components/AdminPage';
-import VideoPlayer from './components/VideoPlayer';
-import SearchPage from './components/SearchPage';
-import ErrorPage from './components/Errorpage';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(Thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route path="/profile/:username" component={Profile} />
-        <Route path="/account" component={Account} />
-        <Route path="/login" component={Login} />
-        <Route path="/createUser" component={CreateUser} />
-        <Route path="/adminpage" component={AdminPage} />
-        <Route path="/video/:videoID" component={VideoPlayer} />
-        <Route path="/video_search/:searchterm" component={SearchPage} />
-        <Route path="/errorpage" component={ErrorPage} />
-      </div>
-    </Router>
+    <App />
   </Provider>, document.getElementById('root'),
 );
