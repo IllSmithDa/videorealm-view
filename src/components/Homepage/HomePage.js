78d86d4a43451098edd7779eb13172a7f86eb192
Samuel Kim <sams404link@gmail.com>
2020-08-18 22:45:44 -0700

    updated homepage

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -9,8 +9,36 @@ export default function HomePage() {
     <div>
       <Navbar />
       <div className="Page-Container">
-        <div className="app-title-container">
-          <h1>Videorealm</h1>
+        <div  id="demo" className="app-title-container carousel slide" data-ride="carousel">
+          <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+          </ul>
+    
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div className="title-container" id="title-slide">
+                <h1>Videorealm</h1>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div className="title-container" id="first-news">
+                <h1>Checkout New Videos</h1>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div className="title-container" id="second-news">
+                <h1>Sign Up for Free</h1>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
         </div>
         <div>
           <h3 className="video-label"><br />Popular Videos </h3>
